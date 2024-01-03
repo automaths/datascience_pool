fa478d8d9465c1553ea3ac1a1ce162aea627f22b
automaths <nicolassartral@gmail.com>
2024-01-03 19:24:32 +0100

    NULL_not_found.py

---
+++ ex03/NULL_not_found.py
@@ -0,0 +1,16 @@
+def NULL_not_found(object: any) -> int:
+    
+    if object is None:
+        print("Nothing:", object, type(object))
+    elif type(object) == float and object != object:
+        print("Cheese:", object, type(object))
+    elif type(object) == int and not object:
+        print("Zero:", object, type(object))
+    elif type(object) == str and not object:
+        print("Empty:", type(object))
+    elif type(object) == bool and not object:
+        print("Fake:", object, type(object))
+    else:
+        print("Type not Found")
+        return 1
+    return 0
